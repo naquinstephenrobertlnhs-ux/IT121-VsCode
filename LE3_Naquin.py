0c3b5080b8b4cccba54ac44239e3ae0d6794656c
Suzuya. <naquin.stephenrobert.lnhs@gmail.com>
2026-02-10 15:03:12 +0800

    ...

---
+++ LE3_Naquin.py
@@ -0,0 +1,20 @@
+Name = "Stephen Robert Z. Naquin"
+age = 18
+Heigth = 1.72
+Currently_Working = True
+
+print("Fullname:",Name)
+print(type(Name))
+print()
+
+print("Age:",age)
+print(type(age))
+print()
+
+print("Heigth_in_meters:", Heigth)
+print(type(Heigth))
+print()
+
+print("Currently_Working?:", Currently_Working)
+print()
+print(type(Currently_Working))
