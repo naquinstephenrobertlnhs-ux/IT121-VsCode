7ca2aa36b327a8752d4308525977c31c15618f92
Suzuya. <naquin.stephenrobert.lnhs@gmail.com>
2026-02-26 14:02:08 +0800

    ...

---
+++ AW3-4_Naquin_#3.py
@@ -0,0 +1,30 @@
+print("Select Operation: ")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+
+while True:
+    choice = input("Enter choice(1/2/3/4): ")
+
+    if choice in ('1', '2', '3', '4'):
+        num1 = int(input("Enter first number: "))
+        num2 = int(input("Enter second number: "))
+
+        if choice == '1':
+            print(num1, "+", num2, "=", num1 + num2)
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", num1 - num2)
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", num1 * num2)
+
+        elif choice == '4':
+            if num2 != 0:
+                print(num1, "/", num2, "=", num1 / num2)
+            else:
+                print("Error: Division by zero is not allowed.")
+        break
+    else:
+        print("Invalid Input")
